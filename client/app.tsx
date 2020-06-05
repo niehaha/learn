5e857a9c5d157d037f92120108773cee51c24777
hopeful <1415129630@qq.com>
2020-06-05 16:53:19 +0800

    "feat: props传值 使用emit"

--- client/app.tsx
+++ client/app.tsx
@@ -2,11 +2,14 @@ import {Vue, Component} from 'vue-property-decorator'
 
 @Component({name:"app"})
 export default class App extends Vue{
+    reload(){
+        console.log('reload');
+    }
     render(){
         return (
             <div>
                 {/* <ssui-header></ssui-header> */}
-                <ssui-map-container minZoom={12}  ></ssui-map-container>
+                <ssui-map-container minZoom={12} reload={this.reload} center={[116.38, 39.90]}></ssui-map-container>
             </div>
         )
     }
--- component/map-container/index.tsx
+++ component/map-container/index.tsx
@@ -1,4 +1,4 @@
-import { Vue, Component, Ref, Prop } from 'vue-property-decorator'
+import { Vue, Component, Ref, Prop,Emit } from 'vue-property-decorator'
 import mapboxGl from 'mapbox-gl'
 import './index.postcss'
 import MapboxLanguage from '@mapbox/mapbox-gl-language'
@@ -6,37 +6,49 @@ import MapboxLanguage from '@mapbox/mapbox-gl-language'
 export default class MapContainer extends Vue {
   @Prop({ type: Number, default: 10 })
   readonly minZoom?: number;
-  @Prop({ type: Array, default: [116.38, 39.90] })
-  readonly center?: [number,number];
+
+  @Prop({ type: Array, default:() => [116.38, 39.90] })
+  readonly center?: [number, number];
+
   @Prop({ type: String, default: 'mapbox://styles/mapbox/streets-v9' })
-  readonly style?: string;
+  readonly mapStyle?: string;
+
   @Prop({ type: Number, default: 3 })
   readonly zoom?: number;
 
   static install(Vue: any) {
     Vue.component('ssui-map-container', MapContainer)
   }
-
   @Ref('map')
   readonly mapContainer!: HTMLDivElement
-
   map?: mapboxGl.Map
 
+  @Emit()
+  reload(){
+    console.log('reload in map component');
+  }
   mounted() {
     mapboxGl.accessToken = 'pk.eyJ1IjoicGlucGFydGRldiIsImEiOiJjajBqOXh0anAwMDFkMzNwbW5qMzVuZGo0In0.ltvQzboVtprxfeFAVOw1GA'
+    const { mapContainer, center, minZoom, zoom, mapStyle } = this
+
     this.map = new mapboxGl.Map({
-      container: this.mapContainer,
-      center: this.center,
-      minZoom: this.minZoom,
-      zoom: this.zoom,
+      container: mapContainer,
+      center: center,
+      minZoom: minZoom,
+      zoom: zoom,
       attributionControl: false,
-      style: this.style
+      style: mapStyle
     })
+    this.map.on('load', () => {
+      console.log('load');
+      this.reload()
+    })
+
     // 设置语言
     let language = new MapboxLanguage({ defaultLanguage: "zh" });
     this.map.addControl(language);
   }
-
+  
   render() {
     return <div ref="map" class='map'></div>
   }
